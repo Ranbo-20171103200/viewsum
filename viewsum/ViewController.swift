a6552723754652c01144b5698fa66d2efe2f124b
Ranbo-20171103200 <1124280263@qq.com>
2018-11-14 10:01:01 +0800

    calculate
    easy calculate

--- viewsum/ViewController.swift
+++ viewsum/ViewController.swift
@@ -9,10 +9,166 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var x: UITextField!
+    @IBAction func sum(_ sender: Any) {
+        if(x.text != ""||cnt1%2==0)
+        {
+            a = Int(x.text!)!
+            c = "+"
+            x.text = ""
+        }
+        if(c=="-"||c=="*"||c=="/")
+        {
+            c = "+"
+        }
+        if(cnt1%2 != 0&&x.text != "")
+        {
+            b = Int(x.text!)!
+            x.text = ""
+            if(c=="+")
+            {
+                b=a+b;
+            }
+            if(c=="-")
+            {
+                b=a-b;
+            }
+            if(c=="*")
+            {
+                b=a*b;
+            }
+            if(c=="/")
+            {
+                b=a/b;
+            }
+        }
+        cnt1 = cnt1 + 1
+    }
+    var a = 0
+    var b = 0
+    var c = "a"
+    var flag = 0
+    var cnt1 = 0
+    @IBAction func num1(_ sender: Any) {
+        x.text = x.text! + "1"
+    }
+    @IBAction func num2(_ sender: Any) {
+        x.text = x.text! + "2"
+    }
+    @IBAction func num3(_ sender: Any) {
+        x.text = x.text! + "3"
+    }
+    @IBAction func num4(_ sender: Any) {
+        x.text = x.text! + "4"
+    }
+    @IBAction func num5(_ sender: Any) {
+        x.text = x.text! + "5"
+    }
+    @IBAction func num6(_ sender: Any) {
+        x.text = x.text! + "6"
+    }
+    @IBAction func num7(_ sender: Any) {
+        x.text = x.text! + "7"
+    }
+    @IBAction func num8(_ sender: Any) {
+        x.text = x.text! + "8"
+    }
+    @IBAction func num9(_ sender: Any) {
+        x.text = x.text! + "9"
+    }
+    @IBAction func num0(_ sender: Any) {
+        x.text = x.text! + "0"
+    }
+    @IBAction func dec(_ sender: Any) {
+        if(a==0&&flag==0)
+        {
+            x.text = x.text! + "-"
+            flag = 1
+        }
+        else
+        {
+            if(x.text != "")
+            {
+                a = Int(x.text!)!
+                c = "-"
+                x.text = ""
+            }
+            if(c=="+"||c=="*"||c=="/")
+            {
+                c="-"
+            }
+        }
+    }
+    @IBAction func mul(_ sender: Any) {
+        if(x.text != "")
+        {
+            a = Int(x.text!)!
+            c = "*"
+            x.text = ""
+        }
+        if(c=="-"||c=="+"||c=="/")
+        {
+            c="*"
+        }
+    }
+    @IBAction func dismul(_ sender: Any) {
+        if(x.text != "")
+        {
+            a = Int(x.text!)!
+            c = "/"
+            x.text = ""
+        }
+        if(c=="+"||c=="*"||c=="-")
+        {
+            c="/"
+        }
+    }
+    @IBAction func result(_ sender: Any) {
+        if(c=="-")
+        {
+            b = -Int(x.text!)!
+            x.text = "\(a+b)"
+        }
+        if(c=="+")
+        {
+            b = Int(x.text!)!
+            x.text = "\(a+b)"
+        }
+        if(c=="*")
+        {
+            b = Int(x.text!)!
+            x.text = "\(a*b)"
+        }
+        if(c=="/")
+        {
+            b = Int(x.text!)!
+            if(b != 0)
+            {
+                x.text = "\(a/b)"
+            }
+        }
+        c="a"
+    }
+    @IBAction func empty(_ sender: Any) {
+        x.text = ""
+        a = 0
+        b = 0
+        c = "a"
+        flag = 0
+    }
+    @IBAction func dot(_ sender: Any) {
+        x.text = x.text! + "."
+    }
+    @IBAction func delet(_ sender: Any) {
+        if(x.text != "")
+        {
+            x.text!.remove(at: x.text!.index(before: x.text!.endIndex))
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
